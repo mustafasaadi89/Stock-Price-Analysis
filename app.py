01e4b96db08c4a459a5882cd3ebfd5198762f98c
Mustafa <mustafa.saadi89@gmail.com>
2018-05-29 19:07:07 -0700

    update 5.29 7:05 PM

--- app.py
+++ app.py
@@ -27,76 +27,52 @@ def history():
     print(stock)
     return render_template('index2.html', stock=stock)
 
-@app.route("/stocks/<stockInput>", methods=['GET'])
-def stocks(stockInput):
-    print('stockInput',stockInput)
 
-    urlroute = f'https://api.iextrading.com/1.0/stock/{stockInput}/batch?types=quote,news,chart&range=2y&last=5'
-
-    r= requests.get(urlroute)
-    response_json = r.json()
-    print(response_json)
-
-    close_value = response_json['quote']['close'];
-    open_value = response_json['quote']['open']
-    company_name = response_json['quote']['companyName']
-    print(response_json['quote']['open'])
 
 @app.route("/stocks/<stockInput>", methods=['GET'])
-#url = `https://api.iextrading.com/1.0/stock/${stock}/batch?types=quote,news,chart&range=2y&last=5`;
-#request
 def stocks(stockInput):
-    print("/n----------------------------------------------")    
-    print('stockInput',stockInput)
-    urlroute = f'https://api.iextrading.com/1.0/stock/{stockInput}/batch?types=quote,news,chart&range=2y&last=5'
-    print('urlroute:', urlroute)
-    r= requests.get(urlroute)
-    response_json = r.json()
-    print(response_json)
-    #close_value = response_json.quote.close;
-    #open_value = response_json.quote.open;
-    close_value = response_json['quote']['close'];
-    open_value = response_json['quote']['open']
-    print(response_json['quote']['open'])
-    print('close_value',close_value)
-    print('open_value', open_value)
-    print('stockInput',stockInput)
+   print('stockInput',stockInput)
 
-    stock = mongo.db.stock
+   urlroute = f'https://api.iextrading.com/1.0/stock/{stockInput}/batch?types=quote,news,chart&range=2y&last=5'
 
-    stock_data = {}
+   r= requests.get(urlroute)
+   response_json = r.json()
+   print(response_json)
 
-    ticker_s = stockInput
-    stock_data["ticker_s"] = ticker_s    
+   close_value = response_json['quote']['close'];
+   open_value = response_json['quote']['open']
+   company_name = response_json['quote']['companyName']
+   print(response_json['quote']['open'])
+   #print('company_name',company_name)
+   
+   stock = mongo.db.stock
 
-    print(stock_data["ticker_s"])
+   stock_data = {}
 
-    m_now = datetime.datetime.now() 
-    stock_data["sdate"] = m_now   
+   ticker_s = stockInput
+   stock_data["ticker_s"] = ticker_s
+   
+   m_now = datetime.datetime.now()
+   stock_data["sdate"] = m_now  
 
-    stock_data["close_value"] = close_value
-    stock_data["open_value"] = open_value
+   stock_data["close_value"] = close_value
+   stock_data["open_value"] = open_value
+   stock_data["company_name"] = company_name
+   
+   today = time.strftime("%m/%d/%Y")
+   today_format = datetime.datetime.strptime(today, "%m/%d/%Y")
+   #print (today_format)
+   
+   exp_date = str(today_format + datetime.timedelta(days=365)).split(" ")
+   ddate = exp_date[0]
+   print (ddate)
 
-    stock_data["company_name"] = company_name
-    
-    today = time.strftime("%m/%d/%Y")
-    today_format = datetime.datetime.strptime(today, "%m/%d/%Y")
-    #print (today_format)
-    
-    exp_date = str(today_format + datetime.timedelta(days=365)).split(" ")
-    ddate = exp_date[0]
-    print (ddate)
+   stock_data["ddate"] = ddate
+   stock.insert({"ticker_s":stock_data["ticker_s"],"company_name":stock_data["company_name"],"close_value":stock_data["close_value"],"open_value":stock_data["open_value"],"ddate":stock_data["ddate"],"sdate":stock_data["sdate"]})
+   
 
-    stock_data["ddate"] = ddate 
-    stock.insert({"ticker_s":stock_data["ticker_s"],"company_name":stock_data["company_name"],"close_value":stock_data["close_value"],"open_value":stock_data["open_value"],"ddate":stock_data["ddate"],"sdate":stock_data["sdate"]})
-
-    print(stock_data["sdate"])
-    print(stock_data["close_value"])
-    print(stock_data["open_value"])
-
-    stock.insert({"ticker_s":stock_data["ticker_s"],"close_value":stock_data["close_value"],"open_value":stock_data["open_value"],"sdate":stock_data["sdate"]})
-
-    return jsonify({'response':'Success'}) 
+   return jsonify({'response':'Success'})
+   #return redirect("http://localhost:5000/", code=302)  
 
     
 if __name__ == "__main__":
